76130b8100ae79b5cac09eb7626c1405bc55888f
Ivana Escobar <ivana@utexas.edu>
2024-06-12 16:24:53 -0500

    ihop cost template from obsfit

--- inc/IHOP_COST.h
+++ inc/IHOP_COST.h
@@ -3,32 +3,59 @@
 !     | o ihop cost terms.                                       |
 !     *==========================================================*
 
-!    IHOP cost flag
+!    OBSFIT logical parameters
+      LOGICAL ihopDoNcOutput
+
+      COMMON /ihop_cost_l/ ihopDoNcOutput
+
+!    IHOP cost integer parameters 
 !     cost_ihop_flag  :: cost ihop flag (see ihop_cost_test.F)
 
       INTEGER cost_ihop_flag
-      COMMON /ihop_cost_i/ cost_ihop_flag
+      INTEGER obs_ind_glob(NFILESMAX_IHOP,NOBSMAX_IHOP)
+      INTEGER ihopOperation(NFILESMAX_IHOP)
+      INTEGER sample_ind_glob(NFILESMAX_IHOP,NSAMPLESMAX,nsx,nsy)
+      INTEGER obs_np(NFILESMAX_IHOP,NOBSMAX_IHOP)
+      INTEGER ObsNo(NFILESMAX_IHOP)
+      INTEGER sampleNo(NFILESMAX_IHOP,nsx,nsy)
+      INTEGER fidfwd_obs(NFILESMAX_IHOP,nsx,nsy)
+      INTEGER fidglobal(NFILESMAX_IHOP)
+      INTEGER obs_sample1_ind(NFILESMAX_IHOP,NOBSMAX_IHOP)
+      COMMON /ihop_cost_i/                                                                                                          &
+     &                  cost_ihop_flag, obs_ind_glob, ihopOperation, obs_np,                                                        &
+     &                  fidfwd_obs, fidglobal, sampleNo,
+     &                  sample_ind_glob, ObsNo, obs_sample1_ind
       
-!    IHOP cost
+!    IHOP cost real parameters
 !     objf_ihop     :: ihop travel times
 !     num_ihop      :: number of observations 
 !     mult_ihop     :: multiplier applied to all cost terms
 
+      _RL  sample_modmask(nsx,nsy)
       _RL  objf_ihop (nSx,nSy)
       _RL  num_ihop  (nSx,nSy)
       _RL  mult_ihop (nSx,nSy)
-      COMMON /IHOP_COST__R/                                                                                                         &
+      _RL obs_modmask
+      _RL obs_delT(NFILESMAX_IHOP,NOBSMAX_IHOP)
+      COMMON /IHOP_COST_R/                                                                                                          &
+     &                sample_modmask,                                                                                               &
      &                objf_ihop,                                                                                                    &
      &                num_ihop,                                                                                                     &
-     &                mult_ihop
+     &                mult_ihop, obs_modmask, obs_delT
 
 !    IHOP cost filenames
 !     ihopObsDir    :: directory where ihop observations are found
 !     ihopObsFile   :: file name for ihop observations 
 
       CHARACTER*(MAX_LEN_FNAM) ihopObsDir
-      CHARACTER*(MAX_LEN_FNAM) ihopObsFile
+      CHARACTER*(MAX_LEN_FNAM) ihopObsFiles(NFILESMAX_IHOP)
       COMMON /IHOP_COST_C/                                                                                                          &
-     &                ihopObsDir, ihopObsFile
+     &                ihopObsDir, ihopObsFiles
+
+      _RL ihop_dummy(NFILESMAX_IHOP,nsx,nsy)
+      _RL ihop_globaldummy(NFILESMAX_IHOP)
+      COMMON /IHOP_CTRL_DUMMY/
+     &                ihop_dummy,
+     &                ihop_globaldummy
 
 !---+----1----+----2----+----3----+----4----+----5----+----6----+----7-|--+----|
--- inc/IHOP_SIZE.h
+++ inc/IHOP_SIZE.h
@@ -60,4 +60,28 @@
 !      PARAMETER( IHOP_NPTS_RANGE = 6 )
 !      INTEGER IHOP_IDW_NPTS
 !      PARAMETER( IHOP_IDW_NPTS = 4 )
+
+
+
+!     Cost function sizes 
+!     ================================
+! NFILESMAX_ihop      :: maximum number of input files
+! NOBSMAX_ihop        :: maximum number of observations per file per tile
+! NSAMPLESMAX        :: maximum number of samples per file per tile
+! NUM_INTERP_PTS_OBS :: number of points used in interpolation for 
+!                       model sampling
+
+#ifdef ALLOW_COST
+      INTEGER NFILESMAX_IHOP
+      PARAMETER ( NFILESMAX_IHOP=3 )
+
+      INTEGER NOBSMAX_IHOP
+      PARAMETER ( NOBSMAX_IHOP=1000 )
+
+      INTEGER NSAMPLESMAX
+      PARAMETER ( NSAMPLESMAX=1000 )
+
+      INTEGER NSAMP_PER_OBS_MAX
+      PARAMETER ( NSAMP_PER_OBS_MAX=1 )
+#endif
 !---+----1----+----2----+----3----+----4----+----5----+----6----+----7-|--+----|
